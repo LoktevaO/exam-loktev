9a593222fb720ca9ed59c595371cbf99ddab960e
LoktevaO <117319338+LoktevaO@users.noreply.github.com>
2022-11-30 20:33:06 +0200

    Update script.sh
    change pach

--- script.sh
+++ script.sh
@@ -1,20 +1,22 @@
-rm -rf exam-loktev
+cd".."
+cd "/var/exam"
+rm -Rf exam-loktev
 git clone git@github.com:LoktevaO/exam-loktev.git
-touch /root/exam-loktev/hosts
+touch /var/exam/exam-loktev/hosts
 cd ".."
 cd "/root/site"
 
 git checkout main
-git clone https://gitlab.com/staskuznetsov/site.git /root/site -b main
-echo 40.113.116.5> /root/exam-loktev/hosts
-ansible-playbook -i /root/exam-loktev/hosts /root/exam-loktev/playbook.yml
+git clone https://gitlab.com/staskuznetsov/site.git /var/exam/exam-loktev/site -b main
+echo 40.113.116.5> /var/exam/exam-loktev/hosts
+ansible-playbook -i /var/exam/exam-loktev/hosts /var/exam/exam-loktev/playbook.yml
 
 git checkout dev
-git clone https://gitlab.com/staskuznetsov/site.git /root/site -b dev
-echo 13.80.138.171> /root/exam-loktev/hosts
-ansible-playbook -i /root/exam-loktev/hosts /root/exam-loktev/playbook.yml
+git clone https://gitlab.com/staskuznetsov/site.git /var/exam/exam-loktev/site -b dev
+echo 13.80.138.171> /var/exam/exam-loktev/hosts
+ansible-playbook -i /root/exam-loktev/hosts /var/exam/exam-loktev/playbook.yml
 
 git checkout feature
-git clone https://gitlab.com/staskuznetsov/site.git /root/site -b feature
-echo 20.107.209.6> /root/exam-loktev/hosts
-ansible-playbook -i /root/exam-loktev/hosts /root/exam-loktev/playbook.yml
+git clone https://gitlab.com/staskuznetsov/site.git /varexam/exam-loktev/site -b feature
+echo 20.107.209.6> /var/exam/exam-loktev/hosts
+ansible-playbook -i /var/exam/exam-loktev/hosts /var/exam/exam-loktev/playbook.yml
